59cb2cc9df8c2b9b5c503922e74866e5a6bbb750
Amrish Fathima F <amchuferoz@gmail.com>
2025-12-23 19:08:58 +0400

    Implement logical operator examples in logicaloper.py
    Added examples for AND, OR, and NOT logical operators.

--- logicaloper.py
+++ logicaloper.py
@@ -1,3 +1,4 @@
+AND:
 high_income = False
 good_credit = True
 
@@ -11,3 +12,22 @@ good_credit = True
 
 if high_income and good_credit:
     print("Eligible")
+
+OR:
+high_income = False
+good_credit = True
+
+if high_income or good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+NOT:
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
